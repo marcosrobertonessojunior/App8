1b0ec67c4bf136a7f8618ee7f70d794817603751
Marcos Nesso <marcos@dittomusic.com>
2018-07-03 15:24:30 -0300

    Created FormCadastro @marcosrobertonessojunior

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import FormLogin from './src/components/FormLogin'
+import FormCadastro from './src/components/FormCadastro'
 
 export default class App extends React.Component {
   render() {
     return (
-      <FormLogin /> 
+      <FormCadastro /> 
     );
   }
 }
